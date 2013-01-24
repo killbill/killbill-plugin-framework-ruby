7585148676570006a85d74c73fd161ab29e49b76
Pierre-Alexandre Meyer <pierre@ning.com>
2013-01-23 21:45:10 -0800

    rake: rework Rake tasks
    Make the directory structure inside the tarball Killbill-friendly, i.e.
    one should just have to untar the plugin inside the bundles/ruby directory for
    Killbill to auto-detect it.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- lib/killbill/rake_task.rb
+++ lib/killbill/rake_task.rb
@@ -11,8 +11,7 @@ module Killbill
     class << self
       def install_tasks(opts = {})
         new(opts[:base_name] || Dir.pwd,                # Path to the plugin root directory (where the gempec and/or Gemfile should be)
-            opts[:target_dir_name] || "build",          # Temporary build directory
-            opts[:name],                                # Plugin name, e.g. 'klogger'
+            opts[:plugin_name],                         # Plugin name, e.g. 'klogger'
             opts[:gem_name],                            # Gem file name, e.g. 'klogger-1.0.0.gem'
             opts[:gemfile_name] || "Gemfile",           # Gemfile name
             opts[:gemfile_lock_name] || "Gemfile.lock") # Gemfile.lock name
@@ -20,18 +19,30 @@ module Killbill
       end
     end
 
-    def initialize(base_name, target_dir_name, name, gem_name, gemfile_name, gemfile_lock_name)
+    def initialize(base_name, plugin_name, gem_name, gemfile_name, gemfile_lock_name)
       @logger = Logger.new(STDOUT)
       #@logger.level = Logger::DEBUG
 
-      # Plugin base directory
-      @base         = Pathname.new(base_name).expand_path
-      # Staging directory
-      @target_dir   = Pathname.new(target_dir_name).expand_path
+      @base_name = base_name
+      @plugin_name = plugin_name
+      @gem_name = gem_name
+      @gemfile_name = gemfile_name
+      @gemfile_lock_name = gemfile_lock_name
 
-      @plugin_gemspec     = find_plugin_gemspec(name)
-      @plugin_gem_file    = find_plugin_gem(gem_name)
-      @gemfile_definition = find_gemfile(gemfile_name, gemfile_lock_name)
+      # Plugin base directory
+      @base = Pathname.new(@base_name).expand_path
+
+      # Find the gemspec to determine name and version
+      @plugin_gemspec = find_plugin_gemspec
+
+      # Temporary build directory
+      # Don't use 'pkg' as it is used by Rake::PackageTask already: it will
+      # hard link all files from @package_dir to pkg to avoid tar'ing up symbolic links
+      @package_dir = Pathname.new(name).expand_path
+
+      # Staging area to install gem dependencies
+      # Note the Killbill friendly structure (which we will keep in the tarball)
+      @target_dir = @package_dir.join("#{version}/gems").expand_path
     end
 
     def specs
@@ -43,29 +54,44 @@ module Killbill
 
     def install
       namespace :killbill do
-        desc "Stage all dependencies"
-        task 'stage' do
-          stage_dependencies
+        desc "Validate plugin tree"
+        task :validate do
+          validate
         end
 
         # Build the .tar.gz and .zip packages
-        Rake::PackageTask.new(name, version) do |p|
-          Rake::Task["stage"].invoke
-
-          p.need_tar = true
+        task :package => :stage
+        package_task = Rake::PackageTask.new(name, version) do |p|
+          p.need_tar_gz = true
           p.need_zip = true
-          # TODO rename build to something else in the tar.gz/zip files
-          p.package_files.include("#{@target_dir.basename}/**/*")
+        end
+
+        desc "Stage all dependencies"
+        task :stage => :validate do
+          stage_dependencies
+
+          # Small hack! Update the list of files to package (Rake::FileList is evaluated too early above)
+          package_task.package_files = Rake::FileList.new("#{@package_dir.basename}/**/*")
         end
 
         desc "List all dependencies"
-        task 'dependency' do
+        task :dependency => :validate do
           print_dependencies
+        end
+
+        desc "Delete #{@package_dir}"
+        task :clean => :clobber_package do
+          rm_rf @package_dir
         end
       end
     end
 
     private
+
+    def validate
+      @plugin_gem_file    = find_plugin_gem
+      @gemfile_definition = find_gemfile
+    end
 
     def print_dependencies
       puts "Gems to be staged:"
@@ -81,25 +107,25 @@ module Killbill
     end
 
     # Parse the <plugin_name>.gemspec file
-    def find_plugin_gemspec(name = nil)
-      gemspecs = name ? [File.join(@base, "#{name}.gemspec")] : Dir[File.join(@base, "{,*}.gemspec")]
+    def find_plugin_gemspec
+      gemspecs = @plugin_name ? [File.join(@base, "#{@plugin_name}.gemspec")] : Dir[File.join(@base, "{,*}.gemspec")]
       raise "Unable to find your plugin gemspec in #{@base}" unless gemspecs.size == 1
       spec_path = gemspecs.first
       @logger.debug "Parsing #{spec_path}"
       Bundler.load_gemspec(spec_path)
     end
 
-    def find_plugin_gem(gem_name = nil)
-      if gem_name
+    def find_plugin_gem
+      if @gem_name
         # Absolute path?
-        plugin_gem_file = Pathname.new(gem_name).expand_path
+        plugin_gem_file = Pathname.new(@gem_name).expand_path
         # Relative path to the base?
-        plugin_gem_file = @base.join(gem_name).expand_path unless plugin_gem_file.file?
-        raise "Unable to find your plugin gem in #{@base}. Did you build it?" unless plugin_gem_file.file?
+        plugin_gem_file = @base.join(@gem_name).expand_path unless plugin_gem_file.file?
+        raise "Unable to find your plugin gem in #{@base}. Did you build it (`rake build')?" unless plugin_gem_file.file?
       else
         plugin_gem_files = Dir[File.join(@base, "**/#{name}-#{version}.gem")]
         @logger.debug "Gem candidates found: #{plugin_gem_files}"
-        raise "Unable to find your plugin gem in #{@base}. Did you build it?" unless plugin_gem_files.size >= 1
+        raise "Unable to find your plugin gem in #{@base}. Did you build it? (`rake build')" unless plugin_gem_files.size >= 1
         # Take the first one, assume the other ones are from build directories (e.g. pkg)
         plugin_gem_file = plugin_gem_files.first
       end
@@ -109,8 +135,8 @@ module Killbill
     end
 
     # Parse the existing Gemfile and Gemfile.lock files
-    def find_gemfile(gemfile_name, gemfile_lock_name)
-      gemfile = @base.join(gemfile_name).expand_path
+    def find_gemfile
+      gemfile = @base.join(@gemfile_name).expand_path
       # Don't make the Gemfile a requirement, a gemspec should be enough
       return nil unless gemfile.file?
 
@@ -118,7 +144,7 @@ module Killbill
       #   Bundler::Installer::install(@target_dir, @definition, {})
       # but it may be better to make sure all dependencies are resolved first,
       # before attempting to build the plugin
-      gemfile_lock = @base.join(gemfile_lock_name).expand_path
+      gemfile_lock = @base.join(@gemfile_lock_name).expand_path
       raise "Unable to find the Gemfile.lock at #{gemfile_lock} for your plugin. Please run `bundle install' first" unless gemfile_lock.file?
 
       @logger.debug "Parsing #{gemfile} and #{gemfile_lock}"
@@ -127,7 +153,7 @@ module Killbill
 
     def stage_dependencies
       # Create the target directory
-      Dir.mkdir @target_dir.to_s
+      mkdir_p @target_dir.to_s
 
       @logger.debug "Installing all gem dependencies to #{@target_dir}"
       # We can't simply use Bundler::Installer unfortunately, because we can't tell it to copy the gems for cached ones
@@ -144,5 +170,3 @@ module Killbill
     end
   end
 end
-
-Killbill::PluginHelper.install_tasks
