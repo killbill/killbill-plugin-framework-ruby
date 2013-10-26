80f0e1602fe1178cf3006ad992417bf7767cc2ad
stephane brossier <stephane@ning.com>
2013-10-26 13:34:08 -0700

    currency plugin work

---
+++ lib/killbill/currency.rb
@@ -0,0 +1,32 @@
+require 'killbill/plugin'
+
+module Killbill
+  module Plugin
+    class Currency < PluginBase
+
+      class OperationUnsupportedError < NotImplementedError
+      end
+
+      def get_base_currencies(options = {})
+        raise OperationUnsupportedError
+      end
+
+      def get_latest_conversion_date(base_currency, options = {})
+        raise OperationUnsupportedError
+      end
+
+      def get_conversion_dates(base_currency, options = {})
+        raise OperationUnsupportedError
+      end
+
+      def get_current_rates(base_currency, options = {})
+        raise OperationUnsupportedError
+      end
+
+      def get_rates(base_currency, conversion_date, options = {})
+        raise OperationUnsupportedError
+      end
+
+    end
+  end
+end
