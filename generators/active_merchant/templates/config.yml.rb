af1bb0bc964c3b5ce79d3dc0d6e493ef85fe3e18
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-08-23 18:31:39 -0700

    generators: add jndi configuration example
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- generators/active_merchant/templates/config.yml.rb
+++ generators/active_merchant/templates/config.yml.rb
@@ -11,3 +11,8 @@
 #  :password: 'killbill'
 #  :driver: 'com.mysql.jdbc.Driver'
 #  :url: 'jdbc:mysql://127.0.0.1:3306/killbill'
+# In Kill Bill
+#  :adapter: 'jdbcmysql'
+#  :jndi: 'killbill/osgi/jdbc'
+#  :driver: 'com.mysql.jdbc.Driver'
+#  :connection_alive_sql: 'select 1'
