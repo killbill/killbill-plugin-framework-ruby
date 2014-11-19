231f85d18cd2f916d0c34a8005639ed53b6af72a
kares <self@kares.org>
2014-11-19 17:21:38 +0100

    avoid AR-JDBC JdbcConnection monkey-patch ... won't work in next major anyways ... in terms of retrying, use `config[:retry_count]` which AR-JDBC supports and shall support for the future

--- generators/active_merchant/templates/config.yml.rb
+++ generators/active_merchant/templates/config.yml.rb
@@ -11,12 +11,13 @@
 #  :password: 'killbill'
 #  :database: 'killbill' # or set the URL :
 #  #:url: jdbc:mysql://127.0.0.1:3306/killbill
-#  :pool: 100 # AR's default is 5
+#  :pool: 100 # AR's default is max 5 connections
 # In Kill Bill
 #  :adapter: mysql
 #  :jndi: 'killbill/osgi/jdbc'
 #  :connection_alive_sql: 'select 1'
 #  :pool: false # false-pool (JNDI pool's max)
+#  :retry_count: 5 # when obtaining from DS fails
 #  # MySQL adapter #configure_connection defaults :
 #  #    @@SESSION.sql_auto_is_null = 0,
 #  #    @@SESSION.wait_timeout = 2147483,
