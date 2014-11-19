c23f7958c12b2c4f9be71a24bc57482efe9205a1
kares <self@kares.org>
2014-11-19 12:52:37 +0100

    review **config.yml** template : - avoid the jdbc prefixed adapter name - deafult to using the false-pool in KB - notes on what happens on configure_connection due AR compatibility (could probably be moved on the KB used JDBC connection pool's side)

--- generators/active_merchant/templates/config.yml.rb
+++ generators/active_merchant/templates/config.yml.rb
@@ -2,16 +2,24 @@
   :test: true
 
 :database:
+# SQLite (development)
   :adapter: sqlite3
   :database: test.db
 # For MySQL
-#  :adapter: 'jdbcmysql'
+#  :adapter: mysql
 #  :username: 'killbill'
 #  :password: 'killbill'
-#  :driver: 'com.mysql.jdbc.Driver'
-#  :url: 'jdbc:mysql://127.0.0.1:3306/killbill'
+#  :database: 'killbill' # or set the URL :
+#  #:url: jdbc:mysql://127.0.0.1:3306/killbill
+#  :pool: 100 # AR's default is 5
 # In Kill Bill
-#  :adapter: 'jdbcmysql'
+#  :adapter: mysql
 #  :jndi: 'killbill/osgi/jdbc'
 #  :connection_alive_sql: 'select 1'
-#  :pool: 250
+#  :pool: false # false-pool (JNDI pool's max)
+#  # MySQL adapter #configure_connection defaults :
+#  #    @@SESSION.sql_auto_is_null = 0,
+#  #    @@SESSION.wait_timeout = 2147483,
+#  #    @@SESSION.sql_mode = 'STRICT_ALL_TABLES'
+#  # this can be disabled since AR-JDBC 1.4 using :
+#  #:configure_connection: false
