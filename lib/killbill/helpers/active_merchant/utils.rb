0d44b8e3d0b2e5dc794aa176dcb315257baf03e4
kares <self@kares.org>
2015-02-13 13:44:41 +0100

    BoundedLRUCache v2 ... simpler - shared API using thread_safe (CHM on JRuby)

--- lib/killbill/helpers/active_merchant/utils.rb
+++ lib/killbill/helpers/active_merchant/utils.rb
@@ -1,3 +1,6 @@
+require 'thread'
+require 'thread_safe'
+
 module Killbill
   module Plugin
     module ActiveMerchant
@@ -46,100 +49,65 @@ module Killbill
           end
         end
 
-        # Relies on the fact that hashes enumerate their values in the order that the corresponding keys were inserted (Ruby 1.9+)
         class BoundedLRUCache
 
-          def initialize(proc, max_size=10000)
-            @proc     = proc
+          def initialize(proc, max_size = 10000)
             @max_size = max_size
+            @data = ThreadSafe::Cache.new do |hash, key|
+              # mapping key -> value is constant for our purposes
+              set_value = hash.fetch_or_store key, value = proc.call(key)
+              store_key(key) if value.equal?(set_value) # very same object
+              set_value
+            end
+            @keys = []
+            @semaphore = Mutex.new
+          end
 
-            if defined?(JRUBY_VERSION)
-              @is_jruby  = true
-              @semaphore = nil
+          def [](key); @data[key] end
 
-              lru_cache = Class.new(java.util.LinkedHashMap) do
-                def initialize(max_size)
-                  super(max_size, 1.0, true)
-                  @max_size = max_size
-                end
+          def []=(key, val)
+            prev_val = @data.get_and_set(key, val)
+            prev_val.nil? ? store_key(key) : update_key(key)
+            val
+          end
 
-                # Note: renaming it to remove_eldest_entry won't work
-                def removeEldestEntry(eldest)
-                  size > @max_size
-                end
-              end.new(@max_size)
-              @data     = java.util.Collections.synchronizedMap(lru_cache)
-            else
-              @is_jruby  = false
-              @semaphore = Mutex.new
-              # TODO Pre-allocate?
-              @data      = {}
+          # @private for testing
+          def size; @data.size end
+
+          # @private for testing
+          def keys
+            @semaphore.synchronize { @keys.dup }
+          end
+
+          # @private for testing
+          def values
+            @semaphore.synchronize { @keys.map { |key| self[key] } }
+          end
+
+          protected
+
+          def store_key(key)
+            @semaphore.synchronize do
+              @keys << key
+              remove_eldest_key_if_full
             end
           end
 
-          def [](key)
-            @is_jruby ? jruby_get(key) : ruby_get(key)
-          end
-
-          def []=(key, val)
-            @is_jruby ? jruby_set(key, val) : ruby_set(key, val)
-          end
-
-          # For testing
-
-          def size
-            @data.size
-          end
-
-          def keys
-            @is_jruby ? @data.key_set.to_a : @data.keys
-          end
-
-          def values
-            @is_jruby ? @data.values.to_a : @data.values
+          def update_key(key)
+            @semaphore.synchronize do
+              @keys.delete(key); @keys << key
+              remove_eldest_key_if_full
+            end
           end
 
           private
 
-          def jruby_get(key)
-            value = @data.get(key)
-            if value.nil?
-              value = @proc.call(key)
-              # Somebody may have beaten us to it but the mapping key -> value is constant for our purposes
-              jruby_set(key, value)
-            end
-            value
+          def remove_eldest_key_if_full
+            @data.delete @keys.shift if @keys.length > @max_size
           end
 
-          def jruby_set(key, val)
-            @data.put(key, val)
-          end
-
-          def ruby_get(key)
-            @semaphore.synchronize do
-              found = true
-              value = @data.delete(key) { found = false }
-              if found
-                @data[key] = value
-              else
-                value      = @proc.call(key)
-                @data[key] = value
-                value
-              end
-            end
-          end
-
-          def ruby_set(key, val)
-            @semaphore.synchronize do
-              @data.delete(key)
-              @data[key] = val
-              if @data.length > @max_size
-                @data.delete(@data.first[0])
-              end
-              val
-            end
-          end
         end
+
       end
     end
   end
