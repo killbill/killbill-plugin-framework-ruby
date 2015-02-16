084f949551b0cbeedfb43b6dc0584d165fca8462
kares <self@kares.org>
2015-02-16 17:53:16 +0100

    review config.yml template + add jdbc-mariadb as dev dependency (for 'real' DB testing)
    - pool: 30 (aligned default with current KB max connections default)
    - specify driver: explicitly so the same JDBC driver is used as in real-life KB
    - connection_alive_sql won't be needed on recent HikariCP/C3P0
    - configure_connection will make sense (esp. in terms of performance) on next AR-JDBC

--- generators/active_merchant/templates/config.yml.rb
+++ generators/active_merchant/templates/config.yml.rb
@@ -11,16 +11,17 @@
 #  :password: 'killbill'
 #  :database: 'killbill' # or set the URL :
 #  #:url: jdbc:mysql://127.0.0.1:3306/killbill
-#  :pool: 100 # AR's default is max 5 connections
+#  :driver: org.mariadb.jdbc.Driver # as in KB
+#  :pool: 30 # AR's default is max 5 connections
 # In Kill Bill
 #  :adapter: mysql
 #  :jndi: 'killbill/osgi/jdbc'
-#  :connection_alive_sql: 'select 1'
 #  :pool: false # false-pool (JNDI pool's max)
-#  :retry_count: 5 # when obtaining from DS fails
+#  # uncomment if pool does not support JDBC4 :
+#  #:connection_alive_sql: 'select 1'
 #  # MySQL adapter #configure_connection defaults :
 #  #    @@SESSION.sql_auto_is_null = 0,
 #  #    @@SESSION.wait_timeout = 2147483,
 #  #    @@SESSION.sql_mode = 'STRICT_ALL_TABLES'
-#  # this can be disabled since AR-JDBC 1.4 using :
-#  #:configure_connection: false
+#  # ... can be disabled (on AR-JDBC 1.4) using :
+#  :configure_connection: false
--- generators/active_merchant/templates/plugin.gemspec.rb
+++ generators/active_merchant/templates/plugin.gemspec.rb
@@ -44,6 +44,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec', '~> 2.12.0'
   if defined?(JRUBY_VERSION)
     s.add_development_dependency 'jdbc-sqlite3', '~> 3.7'
+    s.add_development_dependency 'jdbc-mariadb', '~> 1.1'
   else
     s.add_development_dependency 'sqlite3', '~> 1.3.7'
   end
