6527eae94daa5b96da9448e84eaab6cb6c757c29
kares <self@kares.org>
2015-02-16 18:04:34 +0100

    [generator] add ar-bogacs dependency + align gem declaration order with gemspec
    ... also activesupport is not really an explicit dependency of most plugin gems

--- generators/active_merchant/templates/plugin.gemspec.rb
+++ generators/active_merchant/templates/plugin.gemspec.rb
@@ -23,21 +23,22 @@ Gem::Specification.new do |s|
   s.rdoc_options << '--exclude' << '.'
 
   s.add_dependency 'killbill', '~> 3.2.0'
-  s.add_dependency 'activemerchant', '~> 1.44.1'
-  s.add_dependency 'offsite_payments', '~> 2.0.1'
-  s.add_dependency 'activerecord', '~> 4.1.0'
-  s.add_dependency 'actionpack', '~> 4.1.0'
-  s.add_dependency 'actionview', '~> 4.1.0'
-  s.add_dependency 'activesupport', '~> 4.1.0'
-  s.add_dependency 'money', '~> 6.1.1'
-  s.add_dependency 'monetize', '~> 0.3.0'
+
   s.add_dependency 'sinatra', '~> 1.3.4'
   s.add_dependency 'thread_safe', '~> 0.3.4'
+  s.add_dependency 'activerecord', '~> 4.1.0'
   if defined?(JRUBY_VERSION)
+    s.add_dependency 'activerecord-bogacs', '~> 0.2.0'
     s.add_dependency 'activerecord-jdbc-adapter', '~> 1.3.12'
     # Required to avoid errors like java.lang.NoClassDefFoundError: org/bouncycastle/asn1/DERBoolean
     s.add_dependency 'jruby-openssl', '~> 0.9.6'
   end
+  s.add_dependency 'actionpack', '~> 4.1.0'
+  s.add_dependency 'actionview', '~> 4.1.0'
+  s.add_dependency 'activemerchant', '~> 1.44.1'
+  s.add_dependency 'offsite_payments', '~> 2.0.1'
+  s.add_dependency 'monetize', '~> 0.3.0'
+  s.add_dependency 'money', '~> 6.1.1'
 
   s.add_development_dependency 'jbundler', '~> 0.4.1'
   s.add_development_dependency 'rake', '>= 10.0.0'
