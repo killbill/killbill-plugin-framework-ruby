559e71def986be78861201da6e9d5961231ac986
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-05-05 07:57:28 -0700

    ext: remove ActiveRecord::Persistence work-around
    The patch is not quite up-to-date with upstream and the
    work-around has never been fully reliable anyways.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/killbill/helpers/active_merchant.rb
+++ lib/killbill/helpers/active_merchant.rb
@@ -3,8 +3,6 @@ module Killbill
     module ActiveMerchant
       require 'killbill'
 
-      require 'killbill/ext/active_merchant/jdbc_connection'
-
       require 'active_support/core_ext'
       require File.dirname(__FILE__) + '/active_merchant/core_ext.rb'
       require File.dirname(__FILE__) + '/active_merchant/configuration.rb'
