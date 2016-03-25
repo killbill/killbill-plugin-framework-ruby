df5d651899278a722a056a023a039fb1ece546b9
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-03-25 15:29:16 -0700

    migration: initial import of killbill-migration tool
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/killbill/migration.rb
+++ lib/killbill/migration.rb
@@ -1,4 +1,7 @@
+require 'highline'
 require 'logger'
+require 'thor'
+
 require 'active_record'
 require 'arjdbc'
 
@@ -6,6 +9,73 @@ require 'jdbc/mariadb'
 Jdbc::MariaDB.load_driver
 
 module Killbill
+
+  class Cli < Thor
+
+    include ::Thor::Actions
+
+    class_option :adapter,
+                 :type => :string,
+                 :default => 'mysql',
+                 :desc => 'Database adapter'
+
+    class_option :driver,
+                 :type => :string,
+                 :default => 'org.mariadb.jdbc.Driver',
+                 :desc => 'Database driver'
+
+    class_option :username,
+                 :type => :string,
+                 :default => 'killbill',
+                 :desc => 'Database username'
+
+    class_option :password,
+                 :type => :string,
+                 :default => 'killbill',
+                 :desc => 'Database password'
+
+    class_option :database,
+                 :type => :string,
+                 :default => 'killbill',
+                 :desc => 'Database name'
+
+    class_option :host,
+                 :type => :string,
+                 :default => '127.0.0.1',
+                 :desc => 'Database hostname'
+
+    desc 'current_version plugin_name', 'Display the current migration version'
+    def current_version(plugin_name)
+      say migration(plugin_name, options).current_version, :green
+    end
+
+    desc 'sql_for_migration plugin_name', 'Display the migration SQL'
+    def sql_for_migration(plugin_name)
+      say migration(plugin_name, options).sql_for_migration.join("\n"), :green
+    end
+
+    desc 'migrate plugin_name', 'Run all migrations'
+    def migrate(plugin_name)
+      migration(plugin_name, options).migrate
+    end
+
+    desc 'ruby_dump plugin_name', 'Dump the current schema structure (Ruby)'
+    def ruby_dump(plugin_name)
+      say migration(plugin_name, options).ruby_dump.string, :green
+    end
+
+    desc 'sql_dump plugin_name', 'Dump the current schema structure (SQL)'
+    def sql_dump(plugin_name)
+      say migration(plugin_name, options).sql_dump.string, :green
+    end
+
+    private
+
+    def migration(plugin_name, options)
+      Killbill::Migration.new(plugin_name, options)
+    end
+  end
+
   class Migration
 
     class << self
@@ -21,7 +91,6 @@ module Killbill
     end
 
     def sql_for_migration(migrations_paths = ActiveRecord::Migrator.migrations_paths)
-      ActiveRecord::Base.connection.migration_statements = []
       ActiveRecord::Base.connection.readonly = true
 
       ActiveRecord::Migrator.migrate(migrations_paths)
@@ -96,7 +165,8 @@ module Killbill
           define_method(method) do |sql, *args|
             migration_sql = /^(create|alter|drop|insert|delete|update)/i.match(sql)
 
-            @migration_statements << "#{sql};\n" if migration_sql
+            @migration_statements ||= []
+            @migration_statements << "#{sql};" if migration_sql
 
             send("old_#{method}", sql, *args) if !migration_sql || (migration_sql && !@readonly)
           end
